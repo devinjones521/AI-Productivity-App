09461443462048f949ed137282721b25e3600719
devinjones521 <devinjones521@gmail.com>
2025-04-26 20:31:48 +0100

    moved get user daily log from core database to services to prevent circular import

--- app/core_database.py
+++ app/core_database.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
 
+
 # Path to your SQLite database file
 DATABASE_URL = "sqlite:///./data/productivity.db"
 
@@ -18,16 +19,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Create a base class for your models to inherit
 Base = declarative_base()
-
-# Dependency to get DB session
-def get_user_log_for_date(user_id: int, target_date, db: Session):
-    """
-    Fetches the user's daily log for a specific date.
-    """
-    return db.query(DailyLog).filter(
-        DailyLog.user_id == user_id,
-        func.date(DailyLog.created_at) == target_date
-    ).first()
 
 # Function to create the database tables
 def get_db():
--- app/services/services.py
+++ app/services/services.py
@@ -1,5 +1,7 @@
-from ..core_database import get_user_log_for_date
+from sqlalchemy.orm import Session
 from .openai_service import summarize_user_log
+from ..models import DailyLog
+
 
 def generate_daily_summary(user_id: int, target_date, db):
     user_log_entry = get_user_log_for_date(user_id=user_id, target_date=target_date, db=db)
@@ -11,3 +13,13 @@ def generate_daily_summary(user_id: int, target_date, db):
 
     summary = summarize_user_log(user_log_text)
     return summary
+
+# Dependency to get DB session
+def get_user_log_for_date(user_id: int, target_date, db: Session):
+    """
+    Fetches the user's daily log for a specific date.
+    """
+    return db.query(DailyLog).filter(
+        DailyLog.user_id == user_id,
+        func.date(DailyLog.created_at) == target_date
+    ).first()
